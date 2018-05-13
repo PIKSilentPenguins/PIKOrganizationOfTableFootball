2a98a18c5498d43f7773521a37237b7d436ebd4a
Wasiollo <wasiak.mk@gmail.com>
2018-05-13 19:12:21 +0200

    css file

--- src/main/java/com/silentpenguins/OpenFoosball/controller/ScoringPageController.java
+++ src/main/java/com/silentpenguins/OpenFoosball/controller/ScoringPageController.java
@@ -3,10 +3,19 @@ package com.silentpenguins.OpenFoosball.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller
 public class ScoringPageController {
     @RequestMapping("/scoring")
-    public String showScoring() {
+    public String showScoring(Map<String, Object> model) {
+        Integer trainingPoints = 0;
+        Integer regularPoints = 1;
+        Integer tournamentPoints = 3;
+        model.put("trainingPoints", trainingPoints);
+        model.put("regularPoints", regularPoints);
+        model.put("tournamentPoints", tournamentPoints);
+
         return "scoring";
     }
 
