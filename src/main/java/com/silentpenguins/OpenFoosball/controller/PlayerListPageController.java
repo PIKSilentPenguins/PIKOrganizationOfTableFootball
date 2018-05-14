3439953a6a5c6fde1a24790c37cd9ad1afde74a3
mwasiak <mwasiak@bluesoft.net.pl>
2018-05-14 17:01:14 +0200

    Lista graczy done Closes #36

--- src/main/java/com/silentpenguins/OpenFoosball/controller/PlayerListPageController.java
+++ src/main/java/com/silentpenguins/OpenFoosball/controller/PlayerListPageController.java
@@ -8,10 +8,10 @@ import java.util.Map;
 import java.util.Vector;
 
 @Controller
-public class RankingController {
+public class PlayerListPageController {
 
-    @RequestMapping("/ranking")
-    public String showRanking(Map<String, Object> model) {
+    @RequestMapping("/player_list")
+    public String showPlayerList(Map<String, Object> model) {
         Vector<Player> playerVector = new Vector<>();
         for(int i =0 ; i< 10 ; ++i){
             Player player1 = new Player();
@@ -21,27 +21,18 @@ public class RankingController {
             player2.setUserName("MPUC");
             player2.setFirstName("Martin");
             player2.setLastName("Putz");
-            player2.setMatches(1000);
-            player2.setPoints(10000);
-            player2.setWins(150);
 
             Player player3 = new Player();
             player3.setId(3);
             player3.setUserName("Pointerrr");
             player3.setFirstName("Piotr");
             player3.setLastName("Komorowski");
-            player3.setMatches(300);
-            player3.setPoints(100);
-            player3.setWins(100);
 
             Player player4 = new Player();
-            player4.setId(3);
+            player4.setId(4);
             player4.setUserName("Elo");
             player4.setFirstName("ELOELOELO");
             player4.setLastName("dlkamsdlasmdk");
-            player4.setMatches(100);
-            player4.setPoints(60);
-            player4.setWins(100);
 
 
             playerVector.add(player1);
@@ -51,6 +42,6 @@ public class RankingController {
         }
         model.put("playerVector", playerVector);
 
-        return "ranking";
+        return "player_list";
     }
 }
